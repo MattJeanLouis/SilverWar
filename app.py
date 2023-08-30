723e537335b17e4f4c8c0798623d14ec45cd56af
EthanPasquier <ethanpasquier.pro@gmail.com>
2023-08-30 19:01:13 +0200

    change

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+import random
+
+
 class Chateau:
     # Initialisation de la classe Chateau
     def __init__(self):
@@ -38,18 +42,35 @@ class Chateau:
         return self.pv <= 0
 
 def decision_bot(chateau_bot, chateau_joueur):
-    # Le bot décide de son action en fonction de l'état de son château et de celui du joueur
-    if chateau_bot.pv < 40:  # Si les points de vie du bot sont bas, il choisit de renforcer
-        return "r"
-    elif chateau_joueur.pv < 40:  # Si les points de vie du joueur sont bas, le bot choisit d'attaquer
-        return "a"
-    else:  # Sinon, le bot active le bouclier
-        return "b"
+    random_factor = random.random()  # Génère un nombre aléatoire entre 0 et 1
+    
+    if chateau_bot.pv < 40:
+        if random_factor < 0.8:  # 80% de chance de renforcer si les points de vie sont bas
+            return "r"
+        else:
+            return "a"
+    elif chateau_joueur.pv < 40:
+        if random_factor < 0.7:  # 70% de chance d'attaquer si les points de vie du joueur sont bas
+            return "a"
+        else:
+            return "b"
+    else:
+        if random_factor < 0.4:  # 40% de chance d'activer le bouclier si les points de vie sont suffisants
+            return "b"
+        else:
+            return "a"
+
 
 def affichage_ascii(chateau1, chateau2):
     # Affichage de l'état des châteaux en utilisant des coeurs pour représenter les points de vie
-    print("Château 1" + "♥" * (chateau1.pv // 10))
-    print("Château 2" + "♥" * (chateau2.pv // 10))
+    if (int(chateau1.pv) // 10) > (int(chateau2.pv) // 10):
+        trait = (int(chateau1.pv) // 10)
+    else :
+        trait = (int(chateau2.pv) // 10)
+    print("--------------------"+ "-" * trait)
+    print("| Votre chateau : " + "♥" * (int(chateau1.pv) // 10))
+    print("| Château Ennemi : " + "♥" * (int(chateau2.pv) // 10))
+    print("--------------------"+ "-" * trait)
 
 def jeu():
     # Initialisation du jeu
@@ -63,7 +84,8 @@ def jeu():
         affichage_ascii(chateau1, chateau2)  # Affichage de l'état des châteaux
 
         # Le joueur choisit son action
-        action = input("Joueur, choisissez une action - attaquer (a), renforcer (r), bouclier (b), piège (p), double attaque (d) : ")
+        action = input("\033[32mChoisissez une action - attaquer (a), renforcer (r), bouclier (b), piège (p), double attaque (d) : \033[31m")
+        print("\033[37m")
         if action == "a":  # Si le joueur choisit d'attaquer
             chateau1.attaquer(chateau2)
         elif action == "r":  # Si le joueur choisit de renforcer
@@ -75,6 +97,8 @@ def jeu():
         elif action == "d":  # Si le joueur choisit de préparer une double attaque
             chateau1.preparer_double_attaque()
 
+
+        os.system("clear")
         # Si le château du bot est détruit, le joueur gagne
         if chateau2.est_detruit():
             print("Château 2 (Bot) est détruit! Joueur gagne!")
@@ -83,18 +107,20 @@ def jeu():
         # Le bot choisit son action
         action_bot = decision_bot(chateau2, chateau1)
         if action_bot == "a":  # Si le bot choisit d'attaquer
-            print("Bot attaque!")
+            print("\033[31mBot attaque!\033[37m")
             chateau2.attaquer(chateau1)
         elif action_bot == "r":  # Si le bot choisit de renforcer
-            print("Bot renforce!")
+            print("\033[31mBot renforce!\033[37m")
             chateau2.renforcer()
         elif action_bot == "b":  # Si le bot choisit d'activer le bouclier
-            print("Bot active le bouclier!")
+            print("\033[31mBot active le bouclier!\033[37m")
             chateau2.activer_bouclier()
 
         # Si le château du joueur est détruit, le bot gagne
         if chateau1.est_detruit():
+            os.system("clear")
             print("Château 1 (Joueur) est détruit! Bot gagne!")
+            
             break
 
         tour += 1  # Incrémentation du compteur de tours
